bbe9e735293fda8a9f5d9091404901f0e6995694
Nicholas Wallace <nicholaslwallace@gmail.com>
2024-03-03 01:11:47 +0000

    Cleaned up `Author` schemas

--- server/objects/entities/Author.js
+++ server/objects/entities/Author.js
@@ -6,13 +6,38 @@ const { checkNamesAreEqual, nameToLastFirst } = require('../../utils/parsers/par
  * @openapi
  * components:
  *   schemas:
+ *     oldAuthorId:
+ *       description: The ID of authors on server version 2.2.23 and before.
+ *       type: string
+ *       format: "aut_[a-z0-9]{18}"
+ *       example: aut_o78uaoeuh78h6aoeif
+ *     newAuthorId:
+ *       type: string
+ *       description: The ID of authors after 2.3.0.
+ *       format: uuid
+ *       example: e4bb1afb-4a4f-4dd6-8be0-e615d233185b
+ *     authorId:
+ *       type: string
+ *       anyOf:
+ *         - $ref: '#/components/schemas/oldAuthorId'
+ *         - $ref: '#/components/schemas/newAuthorId'
+ *     authorSeries:
+ *       type: object
+ *       properties:
+ *         id:
+ *           $ref: '#/components/schemas/seriesId'
+ *         name:
+ *           $ref: '#/components/schemas/seriesName'
+ *         items:
+ *           description: The items in the series. Each library item's media's metadata will have a `series` attribute, a `Series Sequence`, which is the matching series.
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/libraryItemMinified'
  *     author:
  *       type: object
  *       properties:
  *         id:
- *           description: The ID of the author.
- *           type: string
- *           example: aut_z3leimgybl7uf3y4ab
+ *           $ref: '#/components/schemas/authorId'
  *         asin:
  *           description: The ASIN of the author. Will be null if unknown.
  *           type: [string, 'null']
@@ -30,45 +55,50 @@ const { checkNamesAreEqual, nameToLastFirst } = require('../../utils/parsers/par
  *           $ref: '#/components/schemas/addedAt'
  *         updatedAt:
  *           $ref: '#/components/schemas/updatedAt'
+ *     authorWithItems:
+ *       type: object
+ *       description: The author schema with an array of items they are associated with.
+ *       allOf:
+ *         - $ref: '#/components/schemas/author'
+ *         - type: object
+ *           properties:
+ *             libraryItems:
+ *               description: The items associated with the author
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/libraryItemMinified'
+ *     authorWithSeries:
+ *       type: object
+ *       description: The author schema with an array of items and series they are associated with.
+ *       allOf:
+ *         - $ref: '#/components/schemas/authorWithItems'
+ *         - type: object
+ *           properties:
+ *             series:
+ *               description: The series associated with the author
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/authorSeries'
  *     authorMinified:
  *       type: object
  *       properties:
  *         id:
- *           description: The ID of the author.
- *           type: string
- *           example: aut_z3leimgybl7uf3y4ab
+ *           $ref: '#/components/schemas/authorId'
  *         name:
  *           description: The name of the author.
  *           type: string
  *           example: Terry Goodkind
  *     authorExpanded:
  *       type: object
- *       properties:
- *         id:
- *           description: The ID of the author.
- *           type: string
- *           example: aut_z3leimgybl7uf3y4ab
- *         asin:
- *           description: The ASIN of the author. Will be null if unknown.
- *           type: [string, 'null']
- *         name:
- *           description: The name of the author.
- *           type: string
- *           example: Terry Goodkind
- *         description:
- *           description: A description of the author. Will be null if there is none.
- *           type: [string, 'null']
- *         imagePath:
- *           description: The absolute path for the author image. Will be null if there is no image.
- *           type: [string, 'null']
- *         addedAt:
- *           $ref: '#/components/schemas/addedAt'
- *         updatedAt:
- *           $ref: '#/components/schemas/updatedAt'
- *         numBooks:
- *           description: The number of books associated with the author in the library.
- *           type: integer
- *           example: 1
+ *       description: The author schema with the total number of books in the library.
+ *       allOf:
+ *         - $ref: '#/components/schemas/author'
+ *         - type: object
+ *           properties:
+ *             numBooks:
+ *               description: The number of books associated with the author in the library.
+ *               type: integer
+ *               example: 1
  */
 class Author {
   constructor(author) {
